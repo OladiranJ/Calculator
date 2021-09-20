2c59ffd25b6aa426465021940c84322f162e1fcf
Oladiran <diran.johnson95@gmail.com>
2021-09-19 21:48:47 -0700

    main function

--- src/Calculator.java
+++ src/Calculator.java
@@ -24,5 +24,11 @@ public class Calculator {
     private JButton resetButton;
     private JButton equalsButton;
 
-
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Calculator");
+        frame.setContentPane(new Calculator().MainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }

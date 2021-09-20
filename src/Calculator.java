026fce67c1c590e392819733d4f7b10964e25915
Oladiran <diran.johnson95@gmail.com>
2021-09-19 23:08:29 -0700

    numbers displayed in text area and action listeners for buttons

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Calculator {
     private JPanel MainPanel;
@@ -23,6 +25,161 @@ public class Calculator {
     private JButton zeroButton;
     private JButton resetButton;
     private JButton equalsButton;
+
+    public Calculator() {
+
+        double num;
+        double answer;
+        int operator;
+
+        public void mathOperations() {
+            switch (operator) {
+                case 1:
+                    answer = num + Double.parseDouble(numberTextArea.getText());
+                    numberTextArea.setText(Double.toString(answer));
+                    break;
+
+                case 2:
+                    answer = num - Double.parseDouble(numberTextArea.getText());
+                    numberTextArea.setText(Double.toString(answer));
+                    break;
+
+                case 3:
+                    answer = num * Double.parseDouble(numberTextArea.getText());
+                    numberTextArea.setText(Double.toString(answer));
+                    break;
+
+                case 4:
+                    answer = num / Double.parseDouble(numberTextArea.getText());
+                    numberTextArea.setText(Double.toString(answer));
+                    break;
+            }
+        }
+
+        zeroButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String zero = zeroButton.getText();
+                numberTextArea.setText(numberTextArea.getText() + zero);
+            }
+        });
+
+        oneButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String one = oneButton.getText();
+                numberTextArea.setText(numberTextArea.getText() + one);
+            }
+        });
+
+        twoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String two = twoButton.getText();
+                numberTextArea.setText(numberTextArea.getText() + two);
+            }
+        });
+
+        threeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String three = threeButton.getText();
+                numberTextArea.setText(numberTextArea.getText() + three);
+            }
+        });
+
+        fourButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String four = fourButton.getText();
+                numberTextArea.setText(numberTextArea.getText() + four);
+            }
+        });
+
+        fiveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String five = fiveButton.getText();
+                numberTextArea.setText(numberTextArea.getText() + five);
+            }
+        });
+
+        sixButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String six = sixButton.getText();
+                numberTextArea.setText(numberTextArea.getText() + six);
+            }
+        });
+
+        sevenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String seven = sevenButton.getText();
+                numberTextArea.setText(numberTextArea.getText() + seven);
+            }
+        });
+
+        eightButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String eight = eightButton.getText();
+                numberTextArea.setText(numberTextArea.getText() + eight);
+            }
+        });
+
+        nineButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nine = nineButton.getText();
+                numberTextArea.setText(numberTextArea.getText() + nine);
+            }
+        });
+
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                numberTextArea.setText("");
+            }
+        });
+
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                num = Double.parseDouble(numberTextArea.getText());
+                operator = 1;
+                numberTextArea.setText("");
+            }
+        });
+
+        subtractButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                num = Double.parseDouble(numberTextArea.getText());
+                operator = 2;
+                numberTextArea.setText("");
+            }
+        });
+
+        multiplyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                num = Double.parseDouble(numberTextArea.getText());
+                operator = 3;
+                numberTextArea.setText("");
+            }
+        });
+
+        divideButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                num = Double.parseDouble(numberTextArea.getText());
+                operator = 4;
+                numberTextArea.setText("");
+            }
+        });
+
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Calculator");

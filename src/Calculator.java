fb02c612f98c8dcab38704e2612e9fbc2f3b1749
Oladiran <diran.johnson95@gmail.com>
2021-09-19 20:42:21 -0700

    panels and calculator text field

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,4 +2,9 @@ import javax.swing.*;
 
 public class Calculator {
     private JPanel MainPanel;
+    private JPanel topPanel;
+    private JLabel titleLabel;
+    private JPanel middlePanel;
+    private JPanel bottomPanel;
+    private JTextArea a0TextArea;
 }
